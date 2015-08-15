269841049ca35efa46478b01b2fd281c2c28da93
Hooman Javidnia <hooman.javidnia@gmail.com>
2015-08-15 04:13:22 -0400

    I am adding some comments to the top of cacheMatrix.R which describes how the functions operate.

--- Assignment 2/cachematrix.R
+++ Assignment 2/cachematrix.R
@@ -1,7 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These are the two main functions that are defined as part of Assignment 2 of R
+# Programming Course on Coursera. This assignment is about lexical scoping.
+# From all that I have read on the forum, it seems that this is more like an 
+# exercise and does not have practial usage at least in the form as is.
 
-## Write a short comment describing this function
+# For this exercise I am following the methodology used in the example given as
+# part of the assignment instructions.
+
+# The first function "makeCacheMatrix()" is going to work as a replacement for
+# the general matrix class. Instead of a matrix, it creates a list of 4
+# functions. These 4 functions are named:
+# 1. set
+# 2. get
+# 3. set.inverse
+# 4. get.inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
