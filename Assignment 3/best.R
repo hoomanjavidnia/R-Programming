76374f810c99f313b3a11122d7489bbe4e58f77b
Hooman Javidnia <hooman.javidnia@gmail.com>
2015-08-17 05:26:30 -0400

    Made conditional statements to stop on error on the wrong state or wrong outcome.

--- Assignment 3/best.R
+++ Assignment 3/best.R
@@ -8,27 +8,44 @@ best <- function(state, outcome) {
     # = Hooman Javidnia                          =
     # ============================================ 
 
-	# Read outcome data
-	# I am using readr package for reading the csv files
-	library(readr)
-    outcome <- read_csv(, col_names = TRUE,
-                             col_types = list(col_date(), col_double(),
-                                              col_double(), col_integer()))
-	# check that state and outcome are valid
+  	# Read outcome data
+  	# I am using readr package for reading the csv files
+  	library(readr)
+    # I don't need to read the whole file as the decision on best hospital is
+    # only based on the mortality rates for "heart attack", "heart failure", and
+    # "pneumonia". We also need the name of the hospital and the state. These
+    # are located in the following columns of the CSV file:
+    # Hospital Name: column 2 (character string)
+    # State: column 7 (factor)
+    # Mortality rate from heart attack: column 11 (double)
+    # Mortality rate from heart failure: column 17 (double)
+    # Mortality rate from pneumonia: column 23 (double)
+    
+    # There is a variable in R which has all the abbreviations of states. I will
+    # use this variable for checking if the name of the variable passed is valid
+    # or not. Also, it will be passed to read_csv file to read the states as a 
+    # factor.
+  
+    # Check  if the state argument is a valid one. There are probably better
+    # ways to do this, but I am just using a simple if.
+    
+    if (!state %in% state.abb) {
+      stop("Invalid state")
+    }
+    
+    # Check if the outcome is one of the valid conditions in the list
+    conditions <- c("heart attack", "heart failure", "pneumonia")
+    # convert the argument to lower case just in case the user has written the
+    # name of the condition using upper case letters or a mixture of upper case
+    # and lower case.
+    if (!tolower(outcome) %in% conditions)
+    
+    
+ #   outcome <- read_csv(, col_names = TRUE,
+ #                            col_types = list(col_date(), col_double(),
+#                                              col_double(), col_integer()))
+    
+    # check that state and outcome are valid
 	
-	# return hospital name in that state with lowest 30-day death rate
-      
-    for (i in id) {
-      # current.file is a data frame which contains the contents of the i-th file
-      # in the id vector.
-      # The column types are specified so that the read_csv's reliance on first
-      # 100 rows doesn't throw off the detection of column types.
-      current.file <- read_csv(files.in.directory[i], col_names = TRUE,
-                               col_types = list(col_date(), col_double(),
-                                                col_double(), col_integer()))
-      # Now that we have the content of i-th file, we make a complete case of 
-      # the current data frame and count number of complete cases.
-      current.file <- current.file[complete.cases(current.file), ]
-      number.of.complete.case <- nrow(current.file)
-      df <- rbind(df, data.frame(id = i, nobs = number.of.complete.case))	
+    # return hospital name in that state with lowest 30-day death rate
 }
