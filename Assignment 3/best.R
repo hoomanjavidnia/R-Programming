70f59aec2546df17e4d912247eb0994ff21afbf7
Hooman Javidnia <hooman.javidnia@gmail.com>
2015-08-23 06:11:23 -0400

    Completed best.R and submitted through submit() function. All three tests were passed.

--- Assignment 3/best.R
+++ Assignment 3/best.R
@@ -22,17 +22,17 @@ best <- function(state, outcome) {
     # Mortality rate from pneumonia: column 23 (double)
     
 
-    outcome <- read_csv("outcome-of-care-measures.csv", 
+    all.outcome.data <- read_csv("outcome-of-care-measures.csv", 
                         col_names = TRUE, 
                         col_types = "_c____c___d_____d_____d_______________________", 
                         na = "NA")
     # Now that the CSV file is read, I am going to convert the state column to
     # factor:
-    outcome$State <- as.factor(outcome$State)
+    all.outcome.data$State <- as.factor(all.outcome.data$State)
 
     # check that state and outcome are valid
     # Extract all the states in the State column of the data frame:
-    all.states <- unique(outcome$State)
+    all.states <- unique(all.outcome.data$State)
     # Check if the state argument is a valid one. There are probably better
     # ways to do this, but I am just using a simple if to throw an error
     # message.
@@ -49,10 +49,27 @@ best <- function(state, outcome) {
     if (!tolower(outcome) %in% conditions) {
         stop("Invalid outcome")
     }
-
+    
     # The above two ifs will take care of the invalid conditions and invalid
     # states. If we get to this point, it means we have the correct state and 
     # correct outcome.
 
     # return hospital name in that state with lowest 30-day death rate
+    # Before starting doing anything, I am going to sort the data frame by state
+    # first and then by the name of the hospital
+    all.outcome.data <- all.outcome.data[order(all.outcome.data$State, 
+                                               all.outcome.data$`Hospital Name`), ]
+    # The reason I reordered the data frame is that this way if there is a tie, 
+    # the first occurence of the min is the answer we are looking for.
+    # First, let's find out which one of the conditions we are looking at:
+    condition.index <- grep(outcome, conditions)
+    # We will need only 3 columns of the data frame: Name of the Hospital, State,
+    # and the column corresponding to the condition we are interested in.
+    # And we are only interested in hospitals in state:
+    all.outcome.data <- all.outcome.data[all.outcome.data$State == state,
+                                         c(1, condition.index + 2)]
+    idx <- which.min(all.outcome.data[, 2])
+    return(all.outcome.data[idx, 1])
+    
+    
 }
